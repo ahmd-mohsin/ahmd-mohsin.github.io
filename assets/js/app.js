da6447d2be59271631618bd61da2eef7df719719
ahmd-mohsin <mmohsin.bee20seecs@seecs.edu.pk>
2024-05-05 18:07:14 +0500

    created forms

--- assets/js/app.js
+++ assets/js/app.js
@@ -138,7 +138,7 @@ let footer = $(`
              <div class="form-header">
                 <h6 class="display">Get in Touch</h6>
               </div>
-                <form name="form1" action="https://formcarry.com/s/BywEPAJNb" method="POST" accept-charset="UTF-8" >
+                <form name="form1" action="https://formspree.io/f/xbjnwqwo" method="POST" accept-charset="UTF-8" >
                   <input id="name" type="text" name="name" placeholder="Your Name" required/>
                   <input id="email" type="email" name="email" placeholder="Email Address" required/>                  
                   <textarea id="textArea" name="message" placeholder="Type your Message" required></textarea>
